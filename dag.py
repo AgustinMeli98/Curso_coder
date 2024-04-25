be94aad15adbfe0d1410eff76ad7cd7158789626
Agustin Meli <158231525+AgustinMeli98@users.noreply.github.com>
2024-04-25 11:31:06 -0300

    Update dag.py

--- dag.py
+++ dag.py
@@ -3,11 +3,10 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 import pandas as pd
-import yfinance as yf
-
+import requests
 
 # Defino la función que ejecutará el script
-def load_bitcoin():
+def load_dollar_data():
 
     # Cargo variables de entorno desde el archivo .env
     load_dotenv()
@@ -28,44 +27,86 @@ def load_bitcoin():
         password=db_password
     )
 
-    # Creo la tabla de Bitcoin si no existe
+    # Creo la tabla de valoracion_monedas si no existe
     with conn.cursor() as cur:
         cur.execute("""
-            CREATE TABLE IF NOT EXISTS bitcoin (
-                datetime TIMESTAMP,
-                "open" FLOAT,
-                "high" FLOAT,
-                "low" FLOAT,
-                "close" FLOAT,
-                "adj_close" FLOAT,
-                "volume" BIGINT
+            CREATE TABLE IF NOT EXISTS valoracion_monedas (
+                fecha TIMESTAMP,
+                moneda VARCHAR,
+                casa VARCHAR,
+                nombre VARCHAR,
+                compra FLOAT,
+                venta FLOAT,
+                fecha_actualizacion TIMESTAMP
             )
         """)
         conn.commit()
-        print("Tabla de Bitcoin creada correctamente.")
+        print("Tabla de valoracion_monedas creada correctamente.")
 
-    # Leo los datos de finanzas de Bitcoin desde un DataFrame de pandas
-    # Símbolo de Bitcoin en Yahoo Finance
-    bitcoin_symbol = "BTC-USD"
+    # Obtengo los datos de la API de Dolarapi
+    response = requests.get("https://dolarapi.com/v1/dolares")
+    data = response.json()
 
-    # Obtengo datos de Bitcoin por hora
-    df = yf.download(bitcoin_symbol, period="1d", interval="1h")
-
-    # Inserto los datos en la tabla de Bitcoin
+    # Inserto los datos en la tabla de valoracion_monedas
     with conn.cursor() as cur:
-        for datetime, row in df.iterrows():
+        for entry in data:
             cur.execute("""
-                INSERT INTO bitcoin ("datetime", "open", "high", "low", "close", "adj_close", "volume")
+                INSERT INTO valoracion_monedas ("fecha", "moneda", "casa", "nombre", "compra", "venta", "fecha_actualizacion")
                 VALUES (%s, %s, %s, %s, %s, %s, %s)
             """, (
-                datetime, row["Open"], row["High"], row["Low"], row["Close"], row["Adj Close"], row["Volume"]
+                datetime.now(), entry["moneda"], entry["casa"], entry["nombre"],
+                entry["compra"], entry["venta"], entry["fechaActualizacion"]
             ))
         conn.commit()
-        print("Datos insertados correctamente en la tabla de Bitcoin.")
+        print("Datos de dolar insertados correctamente en la tabla de valoracion_monedas.")
+ # Obtengo los datos de la API para la cotización del Euro
+    response = requests.get("https://dolarapi.com/v1/cotizaciones/eur")
+
+    # Verifica el estado de la respuesta
+    if response.status_code == 200:
+        # Convierte la respuesta en un diccionario
+        data = response.json()
+
+        # Inserto los datos del Euro en la tabla de valoracion_monedas
+        with conn.cursor() as cur:
+            cur.execute("""
+                INSERT INTO valoracion_monedas ("fecha", "moneda", "casa", "nombre", "compra", "venta", "fecha_actualizacion")
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """, (
+                datetime.now(), data["moneda"], data["casa"], data["nombre"],
+                data["compra"], data["venta"], data["fechaActualizacion"]
+            ))
+            conn.commit()
+            print("Datos del Euro insertados correctamente en la tabla de valoracion_monedas.")
+    else:
+        print("Error al obtener los datos del Euro:", response.status_code)
+
+    # Obtengo los datos de la API para la cotización del Peso Chileno
+    response = requests.get("https://dolarapi.com/v1/cotizaciones/clp")
+
+    # Verifica el estado de la respuesta
+    if response.status_code == 200:
+        # Convierte la respuesta en un diccionario
+        data = response.json()
+
+        # Inserto los datos del Peso Chileno en la tabla de valoracion_monedas
+        with conn.cursor() as cur:
+            cur.execute("""
+                INSERT INTO valoracion_monedas ("fecha", "moneda", "casa", "nombre", "compra", "venta", "fecha_actualizacion")
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """, (
+                datetime.now(), data["moneda"], data["casa"], data["nombre"],
+                data["compra"], data["venta"], data["fechaActualizacion"]
+            ))
+            conn.commit()
+            print("Datos del Peso Chileno insertados correctamente en la tabla de valoracion_monedas.")
+    else:
+        print("Error al obtener los datos del Peso Chileno:", response.status_code)
 
     # Cierro la conexión
     conn.close()
 
 
-# Llamo a la función para cargar los datos de Bitcoin
-load_bitcoin()
+# Llamo a la función para cargar los datos de Dolar
+load_dollar_data()
+
